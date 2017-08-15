f1add0ba2d0f9d5e10532935fc0405b9be7d1b98
vagrant <vagrant@homestead>
2017-08-15 05:22:17 +0000

    Add user following

--- app/Models/User.php
+++ app/Models/User.php
@@ -61,8 +61,11 @@ class User extends Model implements AuthenticatableContract,
 
     public function feed()
     {
-        return $this->statuses()
-                    ->orderBy('created_at', 'desc');
+        $user_ids = Auth::user()->followings->pluck('id')->toArray();
+        array_push($user_ids, Auth::user()->id);
+        return Status::whereIn('user_id', $user_ids)
+                              ->with('user')
+                              ->orderBy('created_at', 'desc');
     }
 
     public function followers()
@@ -94,15 +97,6 @@ class User extends Model implements AuthenticatableContract,
     public function isFollowing($user_id)
     {
         return $this->followings->contains($user_id);
-    }
-
-    public function feed()
-    {
-        $user_ids = Auth::user()->followings->pluck('id')->toArray();
-        array_push($user_ids, Auth::user()->id);
-        return Status::whereIn('user_id', $user_ids)
-                              ->with('user')
-                              ->orderBy('created_at', 'desc');
     }
 
 }
